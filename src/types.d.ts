7c9d4eaa9b826fb79b3cc2e944b34361ee992be6
Ken Tandrian <60643640+KenTandrian@users.noreply.github.com>
2023-08-29 05:51:03 +0000

    feat(ts): create IPost interface

---
+++ src/types.d.ts
@@ -0,0 +1,10 @@
+interface IPost {
+    title: string;
+    date: string;
+    author?: string;
+    image: string;
+    excerpt: string;
+    isFeatured: boolean;
+    content: string;
+    slug: string;
+}
